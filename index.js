57d6474204d5adc790c292767a1d848a7166cd06
Amitdeep Brar <754202@student.tor.loc>
2019-06-19 16:38:46 -0400

    4:38 commit

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-const circle = {
-radius: 1,
-location:{
 
-x: 1,
-y: 1
-
-},
-draw: function() {
-    console.log('draw');
-}
-};
-circle.draw();
+// factory function
+function createCircle(radius){
+    return {
+       radius,
+       draw: function(){
+           console.log('draw');
+       }
+   };   
+   }
+   const circle = createCircle(1);
+   circle.draw();
+   
+   // Constructor Function
+   
+   function Circle(radius){
+       console.log('this', this);
+       this.radius = radius;
+       this.draw = function(){
+           console.log("draw");
+       }
+   }
+   
+   const another = new Circle(1);
